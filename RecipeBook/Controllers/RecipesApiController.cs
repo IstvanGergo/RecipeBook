b80127d351c83826a650ae0d017d034694845f25
IstvanGergo <istvangergo96@gmail.com>
2025-11-20 21:09:37 +0100

    feat: add API Controller for React

---
+++ RecipeBook/Controllers/RecipesApiController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeBook.Data;
+using RecipeBook.ViewModels;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RecipeBook.Controllers
+{
+    [Route( "api/[controller]" )]
+    [ApiController]
+    public class RecipesApiController : ControllerBase
+    {
+        private readonly RecipeDbContext _context;
+
+        public RecipesApiController( RecipeDbContext context )
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RecipeViewModel>>> GetRecipes()
+        {
+            var recipes = await _context.Recipes
+                .Include(r => r.tags)
+                .Include(r => r.quantities)
+                    .ThenInclude(q => q.ingredient)
+                .Include(r => r.quantities)
+                    .ThenInclude(q => q.measurement)
+                .Include(r => r.recipe_steps)
+                .ToListAsync();
+
+            var recipeViewModels = recipes.Select(r => new RecipeViewModel(r)).ToList();
+
+            return Ok( recipeViewModels );
+        }
+    }
+}
